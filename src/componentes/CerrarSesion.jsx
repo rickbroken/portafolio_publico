b002dc529983b2b46bcf5fa903d4eea1ca42c857
rickbroken <daironquebrada@gmail.com>
2024-10-31 11:35:03 -0500

    feature / se agrega validacion de modal para confirmacion al cerrar sesion

--- src/componentes/CerrarSesion.jsx
+++ src/componentes/CerrarSesion.jsx
@@ -2,6 +2,7 @@ import { Icon } from '@iconify/react';
 import React from 'react';
 import { auth } from '../firebase/firebaseConfig';
 import { signOut } from 'firebase/auth';
+import Swal from 'sweetalert2';
 
 
 const CerrarSesion = () => {
@@ -15,8 +16,25 @@ const CerrarSesion = () => {
     }
   }
 
+  const cerrarSesionModal = () => {
+    Swal.fire({
+      title: "¿Seguro que deseas cerrar sesion?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, Cerrar Sesion",
+      cancelButtonText: "No, Quedarme :)",
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        cerrarSesion();
+      }
+    });
+  }
+
   return (
-    <button onClick={()=>cerrarSesion()} className='bg-[#9c3b4b] hover:bg-[#852635] active:bg-[#77222e] w-36 py-1 text-sm rounded-md flex justify-center items-center font-[200]'>
+    <button onClick={()=>cerrarSesionModal()} className='bg-[#9c3b4b] hover:bg-[#852635] active:bg-[#77222e] w-36 py-1 text-sm rounded-md flex justify-center items-center font-[200]'>
       <Icon icon="majesticons:door-exit-line" width='18' className='mr-1'/>Cerrar Sesion
     </button>
   );
