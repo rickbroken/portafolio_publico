83d81eedf1ff219f2e0fa188f8d25cc1815dedf1
rickbroken <daironquebrada@gmail.com>
2025-01-29 09:07:14 -0500

    feature / se modifican iconos y redes sociales

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -16,40 +16,44 @@ import { useAuth } from '../contextos/useAuth'
 
 const Header = () => {
   const [config, setConfig] = useState(false);
-  const {perfil} = useObtenerPerfil();
-  const {usuario} = useAuth();
+  const { perfil } = useObtenerPerfil();
+  const { usuario } = useAuth();
 
   return (
     <>
       {config &&
-        <ConfigPerfil setConfig={setConfig} perfil={perfil}/>
+        <ConfigPerfil setConfig={setConfig} perfil={perfil} />
       }
-			<ContenedorBannerHeader>
-				<img className='object-cover w-full' src={bannerPerfil} alt="bannerPerfil" />
-			</ContenedorBannerHeader>
+      <ContenedorBannerHeader>
+        <img className='object-cover w-full' src={bannerPerfil} alt="bannerPerfil" />
+      </ContenedorBannerHeader>
 
-			<ContenedorDescripcionPerfil>
+      <ContenedorDescripcionPerfil>
 
-				<ContenedorImgPerfil>
+        <ContenedorImgPerfil>
           {perfil.length !== 0 ?
-            <ImgPerfil ImagenPerfil={perfil[0]?.urlImagenPerfil} id={perfil[0]?.id}/>
+            <ImgPerfil ImagenPerfil={perfil[0]?.urlImagenPerfil} id={perfil[0]?.id} />
             : <div className='h-[150px] w-[150px] sm:h-[188px] sm:w-[188px] bg-slate-600 rounded-full animate-pulse my-1'></div>
           }
-				</ContenedorImgPerfil>
+        </ContenedorImgPerfil>
 
-				<ContenedorTextoPerfil>
+        <ContenedorTextoPerfil>
           <div className='flex justify-between w-full'>
-            {perfil.length !== 0 ? 
-              <p className='font-primaria font-[400] text-3xl' >{perfil[0].nombres +' '+ perfil[0].apellidos}</p>
+            {perfil.length !== 0 ?
+              <p className='font-primaria font-[400] text-3xl' >{perfil[0].nombres + ' ' + perfil[0].apellidos}</p>
               :
               <div className='h-8 w-8/12 bg-slate-600 rounded-md animate-pulse my-1'></div>
             }
-            {usuario !== null && <Icon onClick={()=>setConfig(true)} className='cursor-pointer' icon="solar:settings-outline" color="white" width="30" />}
+            {usuario !== null && <Icon onClick={() => setConfig(true)} className='cursor-pointer' icon="solar:settings-outline" color="white" width="30" />}
           </div>
           {perfil.length !== 0 ?
             <>
-              <p className='font-primaria font-[200] text-[#acacac]'>{perfil[0].habilidades}</p>
-              <p className='font-primaria font-[300] my-2 text-[#d1d1d1]'>{perfil[0].residencia}</p>
+              <p className='font-primaria font-[200] text-[#acacac] mb-2'>{perfil[0].habilidades}</p>
+              <ContenedorRedesSocialesPerfil
+                icon='skill-icons:discord'
+                name={perfil[0].residencia}
+                url={'https://discord.gg/dNavpKnJUM'}
+              />
             </>
             :
             <>
@@ -57,57 +61,57 @@ const Header = () => {
               <div className='h-6 w-3/12 bg-slate-600 rounded-md animate-pulse my-1'></div>
             </>
           }
-					<div className='flex my-4 flex-wrap'>
-          {perfil.length !== 0 ?
-            <>
-              {perfil[0]?.github !== '' &&
-                <ContenedorRedesSocialesPerfil 
-                  icon='fa:github-square'
-                  name='GitHub'
-                  url={perfil[0].github}
-                /> 
-              }
-              {perfil[0]?.linkedin !== '' &&
-                <ContenedorRedesSocialesPerfil 
-                  icon='bi:linkedin'
-                  name='Linkedin'
-                  url={perfil[0].linkedin}
-                />
-              }
-              {perfil[0]?.instagram !== '' &&
-                <ContenedorRedesSocialesPerfil 
-                  icon='bi:instagram'
-                  name='Instagram'
-                  url={perfil[0].instagram}
-                />
-              }
-              {perfil[0]?.figma !== '' &&
-                <ContenedorRedesSocialesPerfil 
-                  icon='ph:figma-logo-duotone'
-                  name='Figma'
-                  url={perfil[0].figma}
-                />
-              }
+          <div className='flex my-4 flex-wrap'>
+            {perfil.length !== 0 ?
+              <>
+                {perfil[0]?.github !== '' &&
+                  <ContenedorRedesSocialesPerfil
+                    icon='fa:github-square'
+                    name='GitHub'
+                    url={perfil[0].github}
+                  />
+                }
+                {perfil[0]?.linkedin !== '' &&
+                  <ContenedorRedesSocialesPerfil
+                    icon='devicon:linkedin'
+                    name='Linkedin'
+                    url={perfil[0].linkedin}
+                  />
+                }
+                {perfil[0]?.instagram !== '' &&
+                  <ContenedorRedesSocialesPerfil
+                    icon='logos:youtube-icon'
+                    name='Canal de YouTube'
+                    url={perfil[0].instagram}
+                  />
+                }
 
-              
-              <div className='flex justify-end group grow'>
-                {usuario !== null && <CerrarSesion />}
+                {perfil[0]?.instagram !== '' &&
+                  <ContenedorRedesSocialesPerfil
+                    icon='logos:tiktok-icon'
+                    name='TikTok'
+                    url={perfil[0].figma}
+                  />
+                }
+
+                <div className='flex justify-end group grow'>
+                  {usuario !== null && <CerrarSesion />}
+                </div>
+              </>
+              :
+              <div className='flex w-full'>
+                <div className='h-5 w-3/12 bg-slate-600 rounded-md animate-pulse mx-1'></div>
+                <div className='h-5 w-3/12 bg-slate-600 rounded-md animate-pulse mx-1'></div>
+                <div className='h-5 w-3/12 bg-slate-600 rounded-md animate-pulse mx-1'></div>
               </div>
-            </>
-            :
-            <div className='flex w-full'>
-              <div className='h-5 w-3/12 bg-slate-600 rounded-md animate-pulse mx-1'></div>
-              <div className='h-5 w-3/12 bg-slate-600 rounded-md animate-pulse mx-1'></div>
-              <div className='h-5 w-3/12 bg-slate-600 rounded-md animate-pulse mx-1'></div>
-            </div>
-          }
+            }
 
-					</div>
-				</ContenedorTextoPerfil>
+          </div>
+        </ContenedorTextoPerfil>
 
-			</ContenedorDescripcionPerfil>
+      </ContenedorDescripcionPerfil>
     </>
   );
 }
- 
+
 export default Header;
