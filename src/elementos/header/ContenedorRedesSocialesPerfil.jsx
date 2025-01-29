727b83177902fb8c128f5c0e73c2bab0974ae1c2
rickbroken <daironquebrada@gmail.com>
2025-01-29 09:07:43 -0500

    feature / se agrega estilo de color blanco a las letras

--- src/elementos/header/ContenedorRedesSocialesPerfil.jsx
+++ src/elementos/header/ContenedorRedesSocialesPerfil.jsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 const ContenedorRedesSocialesPerfil = ({icon,name,url}) => {
   return (
 		<a href={url} target='_blank' className='flex mr-4 items-center cursor-pointer'>
-			<Icon className='mr-1' icon={icon} width='23' color='#92a4af'/>
+			<Icon className='mr-1' icon={icon} width='23' color='#fff'/>
 			<p className='font-primaria font-[200]'>{name}</p>
 		</a>
 	);
