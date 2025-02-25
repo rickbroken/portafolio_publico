89f93712e269d34b935b55c13fe82965bc9cb293
rickbroken <daironquebrada@gmail.com>
2025-02-25 10:44:36 -0500

    fix: update useObtenerPerfil hook to remove unused code and improve performance

--- src/componentes/ConfigPerfil.jsx
+++ src/componentes/ConfigPerfil.jsx
@@ -79,7 +79,7 @@ const ConfigPerfil = ({ setConfig, perfil }) => {
               </div>
 
               <div className='w-[92%]'>
-                <p>Youtube:</p>
+                <p>Instagram:</p>
                 <input className='py-2 px-3 rounded-sm outline-none mb-5 w-full' type="text" value={instagram} onChange={(e) => setInstagram(e.target.value)} />
               </div>
               <div className='w-[92%]'>
