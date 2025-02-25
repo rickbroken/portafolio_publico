c90c9c90e738ff24134de748edbdc6a440a355f9
rickbroken <daironquebrada@gmail.com>
2025-02-25 10:50:55 -0500

    chore: update social media links styling and icons

--- src/elementos/header/ContenedorRedesSocialesPerfil.jsx
+++ src/elementos/header/ContenedorRedesSocialesPerfil.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const ContenedorRedesSocialesPerfil = ({icon,name,url}) => {
   return (
-		<a href={url} target='_blank' className='flex mr-4 items-center cursor-pointer'>
+		<a href={url} target='_blank' className='flex mr-5 items-center cursor-pointer'>
 			<Icon className='mr-1' icon={icon} width='23' color='#fff'/>
 			<p className='font-primaria font-[200]'>{name}</p>
 		</a>
